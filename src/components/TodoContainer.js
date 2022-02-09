ac058b5010d2ec74aa596c13e2324f718e7c475f
jihaneh <jijih@hotmail.fr>
2022-02-09 17:47:01 +0100

    Add header

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TodosList from './TodosList';
+import Header from './Header';
 
 class TodoContainer extends React.Component {
   constructor(props) {
@@ -31,6 +32,7 @@ class TodoContainer extends React.Component {
 
     return (
       <div>
+        <Header />
         <TodosList todos={todos} />
       </div>
     );
